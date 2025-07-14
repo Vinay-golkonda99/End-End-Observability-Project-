67f1d0e54dbe85af7fb02c9c3e068dd4fbc887df
Vinay-golkonda99 <122544244+Vinay-golkonda99@users.noreply.github.com>
2025-07-14 15:46:03 +0530

    Update run.sh

--- app/run.sh
+++ app/run.sh
@@ -1,5 +1,5 @@
-export OTEL_EXPORTER_OTLP_ENDPOINT="http://otel-collector.observability:4317"
-export OTEL_SERVICE_NAME="your-service-name"
+export OTEL_EXPORTER_OTLP_ENDPOINT="http://otel-collector.otel-demo:4317"
+export OTEL_SERVICE_NAME="my-llm-app"
 # Any other OTEL env vars you need
 
 gunicorn -w 1 --threads 4 -b unix:/tmp/gunicorn.sock --timeout 120 main:app &
